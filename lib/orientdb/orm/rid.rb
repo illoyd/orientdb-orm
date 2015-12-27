c00a200080f8b759e18a2bc5dfaf5d3e6ee36b20
Ian Lloyd <ian.w.lloyd@gmail.com>
2015-12-26 20:51:23 -0500

    RID comparable

--- lib/orientdb/orm/rid.rb
+++ lib/orientdb/orm/rid.rb
@@ -11,15 +11,45 @@ module Orientdb
         @collection, @position = collection, position
       end
       
-      def <=>(other)
-        return nil unless self.class == other.class
-        self.to_s <=> other.to_s
-      end
-      
+      ##
+      # Get the object referenced by this RID.
       def fetch
         Orientdb::ORM::Queries::Select.new.from(self).execute.first
       end
 
+      ##
+      # Determines if this RID is valid.
+      def valid?
+        !@collection.nil? && !@position.nil?
+      end
+  
+      ##
+      # Determines if this RID is persisted (both collection and position must be greater than or equal to 0).
+      # A negative in either collection or position implies an object not yet persisted.
+      def persisted?
+        valid? && @collection >= 0 && @position >= 0
+      end
+  
+      ##
+      # Convert to a canonical RID format.
+      def to_s
+        "##{ @collection || 'NIL' }:#{ @position || 'NIL' }"
+      end
+  
+      ##
+      # Convert to the string RID, without the preceeding #.
+      def to_short_s
+        "#{ @collection || 'NIL' }:#{ @position || 'NIL' }"
+      end
+  
+      ##
+      # Pretty print this object.
+      def inspect
+        "<RID #{ to_s }>"
+      end
+  
+      ##
+      # Coercion to a RID.
       def self.call(value)
         case value
         when RID
@@ -34,34 +64,24 @@ module Orientdb
           new(-1, -1)
       end
   
-      def valid?
-        !@collection.nil? && !@position.nil?
+      ##
+      # Comparable function.
+      def <=>(other)
+        return nil unless other.respond_to?(:collection) && other.respond_to?(:position)
+        self.collection <=> other.collection && self.position <=> other.position
       end
-  
-      def persisted?
-        valid? && @collection >= 0 && @position >= 0
-      end
-  
-      def to_s
-        "##{ @collection || 'NIL' }:#{ @position || 'NIL' }"
-      end
-  
-      def to_short_s
-        "#{ @collection || 'NIL' }:#{ @position || 'NIL' }"
-      end
-  
-      def inspect
-        "<RID #{ to_s }>"
-      end
-  
+      
+      ##
+      # TODO Replace with <=> function.
       def ==(other)
-        case other
+        return case other
         when RID
           collection == other.collection && position == other.position
         else
           self == RID.call(other)
         end
       end
+
     end
   end
 end
