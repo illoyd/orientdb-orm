48fdeda0ee144774417ed7a14a564f53e7240546
Ian Lloyd <ian.w.lloyd@gmail.com>
2015-12-31 19:18:52 -0500

    Implement V.all spec

--- spec/orientdb/orm/v_finders_spec.rb
+++ spec/orientdb/orm/v_finders_spec.rb
@@ -5,6 +5,26 @@ describe Orientdb::ORM::Finders, :with_database do
   class OUser < Orientdb::ORM::V; end
   
   describe OUser do
+    
+    describe '.all' do
+
+      it 'returns a Result object' do
+        expect( described_class.all ).to be_an(Orientdb::ORM::Queries::Result)
+      end
+      
+      it 'returns all objects' do
+        expect( described_class.all.count ).to eq(3)
+      end
+      
+      it 'returns only OUser objects' do
+        expect( described_class.all.map { |obj| obj.class } ).to match_array([ OUser, OUser, OUser ])
+      end
+      
+      it 'returns expected RIDs' do
+        expect( described_class.all.map { |obj| obj._rid.to_s } ).to match_array([ '#5:0', '#5:1', '#5:2' ])
+      end
+
+    end
 
     describe '.where' do
       
@@ -52,7 +72,7 @@ describe Orientdb::ORM::Finders, :with_database do
         end
       end
   
-    end # .find_by
+    end # .where
 
     describe '.find_by' do
       
@@ -78,17 +98,17 @@ describe Orientdb::ORM::Finders, :with_database do
   
     end # .find_by
 
-    describe '.find_by_id' do
+    describe '.find' do
       
       context 'with a known OUser' do
         it 'finds the OUser' do
-          expect( described_class.find_by_id('#5:0') ).to be_a(described_class)
+          expect( described_class.find('#5:0') ).to be_a(described_class)
         end  
       end
   
       context 'with a known non-object' do
         it 'returns nil' do
-          expect( described_class.find_by_id('#5000:0') ).to be_nil
+          expect( described_class.find('#5000:0') ).to be_nil
         end
       end
   
