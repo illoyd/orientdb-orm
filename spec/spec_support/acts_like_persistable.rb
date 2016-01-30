cd5ae20234b7e1aa1e533fd87ec785412e6687f0
Ian Lloyd <ian.w.lloyd@gmail.com>
2016-01-30 11:31:33 +0000

    Improve persistence tests

--- spec/spec_support/acts_like_persistable.rb
+++ spec/spec_support/acts_like_persistable.rb
@@ -12,8 +12,8 @@ shared_examples 'a persistable object' do
       link:      Orientdb::ORM::RID.new(5, 0),
       date:      Date.new(2016, 1, 23),
       date_time: Time.new(2016, 1, 23, 8, 22, 54),
-      linklist:  [ Orientdb::ORM::RID.new(5, 0) ],
-      linkset:   Set.new([ Orientdb::ORM::RID.new(5, 0) ]),
+      linklist:  [ Orientdb::ORM::RID.new(5, 0), Orientdb::ORM::RID.new(5, 1) ],
+      linkset:   Set.new([ Orientdb::ORM::RID.new(5, 0), Orientdb::ORM::RID.new(5, 1) ]),
       linkmap:   { hello: Orientdb::ORM::RID.new(5, 0) },
       fieldtypes: Orientdb::ORM::FieldType.new({ '@rid' => Orientdb::ORM::LinkType }),
     }
@@ -33,6 +33,52 @@ shared_examples 'a persistable object' do
         expect( subject.value ).to match v
       end
 
+    end
+
+    it 'persists a string with line breaks' do
+      subject.schema << Orientdb::ORM::AttributeDefinition.new(:value, :string)
+      value = 'Hello!
+      I am a multiline entry.'
+      subject[:value] = value
+
+      # Save subject
+      expect{ subject.save! }.to change(subject, :persisted?).to(true)
+
+      # Reload subject
+      subject.reload!
+
+      # Check that value is returned
+      expect( subject.value ).to match value
+    end
+
+    it 'persists a string with single quotes' do
+      subject.schema << Orientdb::ORM::AttributeDefinition.new(:value, :string)
+      value = 'Hello! I contain \'single quotes\'.'
+      subject[:value] = value
+
+      # Save subject
+      expect{ subject.save! }.to change(subject, :persisted?).to(true)
+
+      # Reload subject
+      subject.reload!
+
+      # Check that value is returned
+      expect( subject.value ).to match value
+    end
+
+    it 'persists a string with double quotes' do
+      subject.schema << Orientdb::ORM::AttributeDefinition.new(:value, :string)
+      value = 'Hello! I contain "double quotes".'
+      subject[:value] = value
+
+      # Save subject
+      expect{ subject.save! }.to change(subject, :persisted?).to(true)
+
+      # Reload subject
+      subject.reload!
+
+      # Check that value is returned
+      expect( subject.value ).to match value
     end
 
   end
